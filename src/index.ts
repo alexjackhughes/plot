898fdbc299aab5f40b287aa4f13b1e515b6d513f
Alex <alex@alexjackhughes.com>
2024-01-23 15:56:21 +0000

    Add async and updated

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,38 @@ import { Socket, newState } from "./utils/state.js"
 import { WebSocketServer } from "ws"
 import { sendLog } from "./utils/logging"
 
-// Define types for clarity
 interface ClientMessage {
-    utc_time: {
-        hour: number,
-        minute: number,
-        second: number,
-    },
-    event_type: number,
-    device_id: string,
-    beacon_id: string,
-    duration: number
+	utc_time: {
+		hour: number,
+		minute: number,
+		second: number,
+	},
+	event_type: number,
+	device_id: string,
+	beacon_id: string, // This is the beacon ID, but we don't know the type of the beacon nor have a way to get this information
+	duration: number // in seconds
 }
 
 interface ServerMessage {
-    device_id: string,
-    haptic_trigger: number,
-    beacon_trigger: number,
-    noise_trigger: number
+	device_id: string,
+	haptic_trigger: number,
+	machine_trigger: number,
+	ppe_trigger: number,
+	access_trigger: number,
+	noise_trigger: number
 }
 
-const wss = new WebSocketServer({ port: Number(process.env.PORT) })
+// Allowed ranges for beacon triggers:
+// ● machine_trigger: 10-15m
+// ● ppe_trigger: 2-3m
+// ● access_trigger: 2 - 3m
 
-wss.on("connection", (ws: Socket) => {
+const wss = new WebSocketServer( { port: Number( process.env.PORT ) } )
+
+wss.on( "connection", async ( ws: Socket ) => {
 	const state = newState( ws )
 
-	ws.on("message", (data) => {
+	ws.on( "message", ( data ) => {
 		const message = data.toString()
 		sendLog( message )
 
@@ -43,24 +49,32 @@ wss.on("connection", (ws: Socket) => {
 			return ws.send( "ERROR: No device_id in message\r\n" );
 		}
 
+		// TODO: If possible, it would be good at this point to use the charging station to register, the devices - but isn't required
+
 		// Send acknowledgment for the received message
-		ws.send("ACK\r\n");
+		ws.send( "ACK\r\n" );
+
+		// TODO: Save the required data here
+
+		// TODO: Fetch the required data here
 
 		// Fixed response message with device settings
 		const response: ServerMessage = {
-				device_id: messageObj.device_id, // Using the same device_id from the client message
-				haptic_trigger: 2, // Fixed value
-				beacon_trigger: 5, // Fixed value
-				noise_trigger: 85 // Fixed value
+			device_id: messageObj.device_id, // Using the same device_id from the client message
+			haptic_trigger: 2, // 2.5 m/s squared (dangerous limit), vibration levels - so in future, this will be different intensity threshold (range and time) for if there's an issue
+			machine_trigger: 10, // in meters
+			ppe_trigger: 3, // in meters
+			access_trigger: 2, // in metersx
+			noise_trigger: 85 // 60 - 120 decibels (dB) max limit
 		};
 
 		// Send the response message with device settings
-		ws.send(JSON.stringify(response));
-	})
+		ws.send( JSON.stringify( response ) );
+	} )
 
-	ws.on("pong", heartbeat)
-	ws.on("close", () => close(state))
-})
+	ws.on( "pong", heartbeat )
+	ws.on( "close", () => close( state ) )
+} )
 
-const interval = keepAlive(wss)
-wss.on("close", () => clearInterval(interval))
+const interval = keepAlive( wss )
+wss.on( "close", () => clearInterval( interval ) )

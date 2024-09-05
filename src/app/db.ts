0339c89aaf3dd8fccfa92c347a72d798f2a78337
Alex <alex@alexjackhughes.com>
2024-09-05 13:15:36 +0100

    Add IMU level

--- src/app/db.ts
+++ src/app/db.ts
@@ -259,6 +259,7 @@ export const addHavEvents = async ({
       severity: 0,
       createdAt: new Date(),
       updatedAt: new Date(),
+      ImuLevel: translateImuLeveltoDBSchema(hav.imu_level),
     };
   });
 
